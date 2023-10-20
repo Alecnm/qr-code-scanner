60023641c86af13a313816b7aa647dc31c287eea
Alec Niño <38384084+Alecnm@users.noreply.github.com>
2023-10-20 22:28:21 +0000

    only camera

--- scanner.js
+++ scanner.js
@@ -4,7 +4,7 @@ function initQRMethod() {
     html5QrCode = new Html5Qrcode("videoScanQR");
     const config = { fps: 10, qrbox: { width: 250, height: 250 } };
   
-    html5QrCode.start({ facingMode: { exact: "environment" } }, config, qrCodeSuccessCallback);
+    html5QrCode.start({ facingMode: { exact: "environment" } }, config);
     // Html5Qrcode.getCameras().then(devices => {
     //   /**
     //    * devices would be an array of objects of type:
@@ -26,15 +26,4 @@ function initQRMethod() {
     
   }
 
-  const qrCodeSuccessCallback = (decodedText, decodedResult) => {
   
-    let urlObject = new URL(decodedText);
-
-    if (urlObject.searchParams.get("token") && urlObject.searchParams.get("u_id") && urlObject.searchParams.get("onlyqr")) {
-      showLoading(decodedText);
-      //console.log(decodedText);
-    } else {
-      qrNotValid();
-      //console.log("QR code is not valid"); // TODO - Show error message to user that QR code is not valid and try again
-    }
-  };
