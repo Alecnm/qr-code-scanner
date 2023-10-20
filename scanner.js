35a58a054907a26b5192be3ddfd3e5eb8eaf71f6
Alec Niño <38384084+Alecnm@users.noreply.github.com>
2023-10-20 22:06:22 +0000

    facing mode environtment

--- scanner.js
+++ scanner.js
@@ -2,26 +2,27 @@
 function initQRMethod() {
 
     html5QrCode = new Html5Qrcode("videoScanQR");
-    Html5Qrcode.getCameras().then(devices => {
-      /**
-       * devices would be an array of objects of type:
-       * { id: "id", label: "label" }
-       */
-      if (devices && devices.length) {
+    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
   
-        if (typeof devices[1] === 'undefined') {
-          var cameraId = devices[0].id;
-        } else {
-          var cameraId = devices[1].id;
-        }
-        console.log(devices);
-        const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+    html5QrCode.start({ facingMode: { exact: "environment" } }, config, qrCodeSuccessCallback);
+    // Html5Qrcode.getCameras().then(devices => {
+    //   /**
+    //    * devices would be an array of objects of type:
+    //    * { id: "id", label: "label" }
+    //    */
+    //   if (devices && devices.length) {
   
-        html5QrCode.start({ deviceId: { exact: cameraId } }, config, qrCodeSuccessCallback);
-      }
-    }).catch(err => {
-      //console.error(err);
-    });
+    //     if (typeof devices[1] === 'undefined') {
+    //       var cameraId = devices[0].id;
+    //     } else {
+    //       var cameraId = devices[1].id;
+    //     }
+    //     console.log(devices);
+        
+    //   }
+    // }).catch(err => {
+    //   //console.error(err);
+    // });
     const qrCodeSuccessCallback = (decodedText, decodedResult) => {
   
       let urlObject = new URL(decodedText);
