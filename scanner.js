6aa922ac6f9fe9d2261bd5d572f00a05213ba848
Alec Niño <38384084+Alecnm@users.noreply.github.com>
2023-10-20 22:36:04 +0000

    jala

--- scanner.js
+++ scanner.js
@@ -2,28 +2,36 @@
 function initQRMethod() {
 
     html5QrCode = new Html5Qrcode("videoScanQR");
-    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+    Html5Qrcode.getCameras().then(devices => {
+      /**
+       * devices would be an array of objects of type:
+       * { id: "id", label: "label" }
+       */
+      if (devices && devices.length) {
   
-    html5QrCode.start({ facingMode: { exact: "environment" } }, config, () => {});
-    // Html5Qrcode.getCameras().then(devices => {
-    //   /**
-    //    * devices would be an array of objects of type:
-    //    * { id: "id", label: "label" }
-    //    */
-    //   if (devices && devices.length) {
+        if (typeof devices[1] === 'undefined') {
+          var cameraId = devices[0].id;
+        } else {
+          var cameraId = devices[1].id;
+        }
+        console.log(devices);
+        const config = { fps: 10, qrbox: { width: 250, height: 250 } };
   
-    //     if (typeof devices[1] === 'undefined') {
-    //       var cameraId = devices[0].id;
-    //     } else {
-    //       var cameraId = devices[1].id;
-    //     }
-    //     console.log(devices);
-        
+        html5QrCode.start({ facingMode: { exact: "environment" } }, config, qrCodeSuccessCallback);
+      }
+    }).catch(err => {
+      //console.error(err);
+    });
+    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+  
+    //   let urlObject = new URL(decodedText);
+  
+    //   if (urlObject.searchParams.get("token") && urlObject.searchParams.get("u_id") && urlObject.searchParams.get("onlyqr")) {
+    //     showLoading(decodedText);
+    //     //console.log(decodedText);
+    //   } else {
+    //     qrNotValid();
+    //     //console.log("QR code is not valid"); // TODO - Show error message to user that QR code is not valid and try again
     //   }
-    // }).catch(err => {
-    //   //console.error(err);
-    // });
-    
+    };
   }
-
-  
