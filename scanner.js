50614a73e7d89319d295a900a84845b7941cd9a7
Alec Niño <38384084+Alecnm@users.noreply.github.com>
2023-10-24 08:39:51 -0500

    Back to facing mode

--- scanner.js
+++ scanner.js
@@ -2,25 +2,25 @@ var html5QrCode = null;
 function initQRMethod() {
 
     html5QrCode = new Html5Qrcode("videoScanQR");
-    let labels = "";
-    Html5Qrcode.getCameras().then(devices => {
-        /**
-         * devices would be an array of objects of type:
-         * { id: "id", label: "label" }
-         */
-        console.log(devices);
-        devices.forEach(device => {
-            alert(device.label);
-        });
-      }).catch(err => {
-        // handle err
-      });
+    // let labels = "";
+    // Html5Qrcode.getCameras().then(devices => {
+    //     /**
+    //      * devices would be an array of objects of type:
+    //      * { id: "id", label: "label" }
+    //      */
+    //     console.log(devices);
+    //     devices.forEach(device => {
+    //         alert(device.label);
+    //     });
+    //   }).catch(err => {
+    //     // handle err
+    //   });
     const qrCodeSuccessCallback = (decodedText, decodedResult) => {
         /* handle success */
     };
     const config = { fps: 10, qrbox: { width: 250, height: 250 } };
 
-    alert(labels);
+    // alert(labels);
     
     // If you want to prefer front camera
     html5QrCode.start({ facingMode: { exact: "environment"} }, config, qrCodeSuccessCallback);
